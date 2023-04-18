e9cde6a98b2007954e7b8e918c74f52985e853da
Adrian Ortiz <adrianortizmartinez@gmail.com>
2023-04-17 21:29:44 -0600

    Created test findAll

--- src/test/java/com/company/springdata/SpringdataApplicationTests.java
+++ src/test/java/com/company/springdata/SpringdataApplicationTests.java
@@ -1,15 +1,31 @@
 package com.company.springdata;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
 
 @SpringBootTest
 @Sql({"/squema.sql", "/data.sql"})
 class SpringdataApplicationTests {
 
+	@Autowired
+	LibroRepository repositoryLibro;
+
 	@Test
-	void contextLoads() {
+	void buscarTodosTest() {
+		Iterable<Libro> it = repositoryLibro.findAll();
+		List<Libro> miLista = new ArrayList<Libro>();
+		// it.forEach(System.out::println);
+		it.forEach(miLista::add);
+
+		assertThat(miLista.size(), greaterThan(6));
 	}
 
 }
